ed4c452af319d6dc3ff174249e37586f2014d437
DEOS <diip.94@mail.ru>
2022-07-07 16:14:56 +0300

    fix: исправлены ошибки rev 1

--- js/index.js
+++ js/index.js
@@ -1,22 +1,15 @@
 // форма
 let formElement = document.forms['popup__form'];
-// поля формы
 let nameInput = formElement.elements['name'];
 let jobInput = formElement.elements['about-me'];
 
 // поля профиля
 let profileName = document.querySelector('.profile__name');
 let profileProfession = document.querySelector('.profile__job');
-// зактырие popUp'a
-let popupClose = document.querySelector('.popup__close');
-// открытие popUp'a
-let editProfile = document.querySelector('.profile__edit');
-let popup = document.querySelector('.popup');
-// Сохранение
-let safeData = document.querySelector('.popup__btn-safe');
 
-// Лайк Active
-let parentsElements = document.querySelector('.elements');
+let popup = document.querySelector('.popup');
+let popupClose = document.querySelector('.popup__close');
+let editProfile = document.querySelector('.profile__edit');
 
 // события
 function formSubmitHandler(e) {
@@ -25,10 +18,7 @@ function formSubmitHandler(e) {
 	profileName.textContent = nameInput.value;
 	profileProfession.textContent = jobInput.value;
 
-	// e.keyCode === 13 ? popup.classList.remove('popup_opened') : false;
-	// if (e.keyCode === 13) {
-	// 	popup.classList.remove('popup_opened');
-	// }
+	closePopupHandler();
 }
 function openPopUpHandler() {
 	popup.classList.add('popup_opened');
@@ -39,18 +29,7 @@ function openPopUpHandler() {
 function closePopupHandler() {
 	popup.classList.remove('popup_opened');
 }
-function safeDataHendler() {
-	popup.classList.remove('popup_opened');
-}
 
 editProfile.addEventListener('click', openPopUpHandler);
 popupClose.addEventListener('click', closePopupHandler);
 formElement.addEventListener('submit', formSubmitHandler);
-safeData.addEventListener('click', safeDataHendler);
-
-// parentsElements.addEventListener('click', putLikeHandler);
-// function putLikeHandler(e) {
-// 	if (e.target.matches('.element__like')) {
-// 		e.target.classList.toggle('element__like_active');
-// 	}
-// }
